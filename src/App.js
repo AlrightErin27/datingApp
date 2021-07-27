4400afcb4a56091a14584df295e0c2027bb87863
AlrightErin27 <erinmontybruce@gmail.com>
2021-07-26 17:12:35 -0700

    thinking thru pages needed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h1>Dating App Log On Page</h1>
+      <div className="toDo">
+      <ol>
+        <li>logon: input for name and password</li>
+        <li>profile page: edit your profile (images & description & what you are looking for), shows who you liked and who liked you</li>
+        <li>nav bar: log out, profile, search/singles, messages</li>
+        <li>shows singles/search: randomly generated with a few restriction, be able to like or send messages</li>
+        <li>logoff</li>
+        <li>messages/inbox</li>
+      </ol>
+      </div>
     </div>
   );
 }
