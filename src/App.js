43fd1e998c6d4d6c8fa07024d0c7474379b7d0d6
AlrightErin27 <erinmontybruce@gmail.com>
2021-07-26 18:03:23 -0700

    added log in form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-
-import './App.css';
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Login from "./container/Login";
 
 function App() {
   return (
     <div className="App">
-      <h1>Dating App Log On Page</h1>
+      <h1>🚧 Dating App Log On Page 🚧</h1>
       <div className="toDo">
-      <ol>
-        <li>logon: input for name and password</li>
-        <li>profile page: edit your profile (images & description & what you are looking for), shows who you liked and who liked you</li>
-        <li>nav bar: log out, profile, search/singles, messages</li>
-        <li>shows singles/search: randomly generated with a few restriction, be able to like or send messages</li>
-        <li>logoff</li>
-        <li>messages/inbox</li>
-      </ol>
+        <ol>
+          <li>logon: input for name and password</li>
+          <li>
+            profile page: edit your profile (images & description & what you are
+            looking for), shows who you liked and who liked you
+          </li>
+          <li>nav bar: log out, profile, search/singles, messages</li>
+          <li>
+            shows singles/search: randomly generated with a few restriction, be
+            able to like or send messages
+          </li>
+          <li>logoff</li>
+          <li>messages/inbox</li>
+        </ol>
       </div>
+
+      {/* ROUTES */}
+      <Login />
     </div>
   );
 }
